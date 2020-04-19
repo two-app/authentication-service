b4bb6a9e5e672815da2a2c99d7e78eebd8a9f4c8
gerryfletch <1997Gerry@gmail.com>
2020-04-19 15:23:37 +0100

    Tested credentials route

--- src/test/scala/credentials/CredentialsRouteTest.scala
+++ src/test/scala/credentials/CredentialsRouteTest.scala
@@ -13,11 +13,17 @@ import db.FlywayHelper
 import scala.reflect.ClassTag
 import spray.json.RootJsonFormat
 import spray.json.DefaultJsonProtocol._
+import tokens.Tokens
+import request.RequestTestArbitraries
+import response.ErrorResponse
+import response.ErrorResponse.{ClientError, InternalError}
+
 class CredentialsRouteTest
     extends AsyncFunSpec
     with Matchers
     with ScalatestRouteTest
-    with BeforeAndAfterEach {
+    with BeforeAndAfterEach
+    with RequestTestArbitraries {
 
   val route: Route = MasterRoute.credentialsRoute
 
@@ -33,38 +39,38 @@ class CredentialsRouteTest
     }
   }
 
-  describe("POST /credentials") {}
+  describe("POST /credentials") {
+    it("should return tokens with valid credentials") {
+      val creds = UserCredentials(1, "testPassword")
+      
+      val tokens = PostCredentials[Tokens](creds)
+
+      extractContext(tokens.accessToken).uid shouldBe creds.uid
+    }
+
+    it("should return a bad request for a small password") {
+      val creds = UserCredentials(1, "bad")
+
+      val error = PostCredentials[ErrorResponse](creds)
+
+      error shouldBe ClientError("Password must be at least six characters in length.")
+    }
+
+    it("should return a bad request for a uid of 0") {
+      val creds = UserCredentials(0, "testPassword")
+
+      val error = PostCredentials[ErrorResponse](creds)
+
+      error shouldBe ClientError("UID must be greater than zero.")
+    }
+
+    it("should return an internal server error for duplicate ids") {
+      val creds = UserCredentials(1, "testPassword")
+
+      PostCredentials[Tokens](creds)
+      val error = PostCredentials[ErrorResponse](creds)
+
+      error shouldBe InternalError()
+    }
+  }
 }
-//   "POST /credentials with valid credentials" should "return 200 OK" in {
-//     postCredentials(1, "testPassword") ~> route ~> check {
-//       response.status shouldBe StatusCodes.OK
-//       val fields = entityAs[String].parseJson.asJsObject.fields
-
-//       fields.contains("accessToken") shouldBe true
-//       fields("accessToken").convertTo[String].length should be > 0
-
-//       fields.contains("refreshToken") shouldBe true
-//       fields("refreshToken").convertTo[String].length should be > 0
-//     }
-//   }
-
-//   "POST /credentials with an invalid UID" should "return a 400 Bad Request" in {
-//     postCredentials(-1, "testPassword") ~> route ~> check {
-//       response.status shouldBe StatusCodes.BadRequest
-//       entityAs[String] shouldBe """{"status":"400 Bad Request","reason":"UID must be greater than zero."}"""
-//     }
-//   }
-
-//   "POST /credentials with a short password" should "return a 400 Bad Request" in {
-//     postCredentials(1, "short") ~> route ~> check {
-//       response.status shouldBe StatusCodes.BadRequest
-//       entityAs[String] shouldBe """{"status":"400 Bad Request","reason":"Password must be at least six characters in length."}"""
-//     }
-//   }
-
-//   def postCredentials(uid: Int, password: String): HttpRequest = {
-//     val credentials = s"""{"uid": $uid, "password": "$password"}"""
-//     Post("/credentials").withEntity(ContentTypes.`application/json`, credentials)
-//   }
-
-// }
