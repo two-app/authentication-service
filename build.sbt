02bfe3beeec1ba63d1fe6ed86339d309b45dd53a
Gerry Fletcher <gerryfletch@users.noreply.github.com>
2020-04-26 19:02:52 +0100

    Kubernetes Deployment (#16)

--- build.sbt
+++ build.sbt
@@ -1,6 +1,6 @@
 name := "authentication-service"
 
-version := "0.1.1"
+version := "0.1.11"
 
 scalaVersion := "2.13.1"
 
@@ -52,3 +52,4 @@ libraryDependencies += "org.typelevel" %% "cats-core" % "2.0.0"
 addCompilerPlugin("org.typelevel" %% "kind-projector" % "0.11.0" cross CrossVersion.full)
 
 dockerRepository := Some("docker.pkg.github.com/two-app/authentication-service")
+dockerUpdateLatest := true
--- src/main/scala/WebServer.scala
+++ src/main/scala/WebServer.scala
@@ -2,15 +2,24 @@ import akka.http.scaladsl.server.{HttpApp, Route}
 import cats.effect.{ExitCode, IO, IOApp}
 import config.MasterRoute
 import config.Config
+import com.typesafe.scalalogging.Logger
 
 class Server extends HttpApp {
   override protected def routes: Route = MasterRoute.masterRoute
 }
 
 object WebServer extends IOApp {
+
+  val logger: Logger = Logger("WebServer")
+
   def run(args: List[String]): IO[ExitCode] = {
+    val host: String = Config.load().getString("server.host")
     val port: Int = Config.load().getInt("server.port")
-    new Server().startServer("localhost", port)
+
+    logger.info(s"Starting server on configured host ${host} and port ${port}.")
+    new Server().startServer(host, port)
+
+    logger.info("Server finished blocking. Exiting with success.")
     IO(ExitCode.Success)
   }
 }
